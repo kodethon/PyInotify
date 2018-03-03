e6dfc5adc43a0feea4b71fd100327a8e8d0fcf5c
Michael Yen <lvlichael8@gmail.com>
2018-03-03 00:20:10 -0800

    Initial commit

--- entrypoint.py
+++ entrypoint.py
@@ -18,34 +18,21 @@ def _main():
         print "Creating %s..." % sys.argv[1] 
         process = subprocess.Popen(['zip', zip_path, '.', '-i', '.'])
     
-    deferred_updates = {}
-    #timestamp = time.time()
-    #backlog = 0
+    delete_backlog = {}
+    update_backlog = {}
+    checkpoint = time.time()
     for event in i.event_gen(yield_nones=False):
-        '''
-        t = time.time()
-
-        if backlog > 0:
-            if time.time() - timestamp > 5:
-                print 'Syncing zip...'
-                sync_zip(zip_path)
-                backlog = 0
-                timestamp = t
-            continue 
-        elif t - timestamp < 0.5:
-            print 'Batching events...'
-            backlog += 1
-            timestamp = t
-            continue
-        '''
-
+        timestamp = time.time()
         (_, type_names, dir_path, filename) = event
         path = os.path.join(dir_path, filename)
-        print "\n{} -> {}".format(type_names, path)
+        print "{} -> {}".format(type_names, path)
         
         event = type_names[0]
         if event == 'IN_IGNORED':
             continue
+
+        print update_backlog
+        print delete_backlog
         
         is_dir = False
         if event == 'IN_ISDIR':
@@ -54,44 +41,40 @@ def _main():
 
         if event == 'IN_DELETE_SELF' or event == 'IN_DELETE' or event == 'IN_MOVED_FROM':
             # If delete operation
-            delete_zip(path, is_dir, zip_path) 
+            delete_backlog[path] = timestamp
+
+            # If most recent operation was deleting a file, don't bother updating
+            if path in update_backlog:
+                update_backlog.pop(path)
             
             # Fix crashing issue...
-            if event == 'IN_DELETE_SELF':
-                i.inotify.remove_watch(path)
+            #if event == 'IN_DELETE_SELF':
+            #    i.inotify.remove_watch(path)
         else:
-            # If update operation
-            if not is_dir:
-                c = os.path.getsize(path)
-                time.sleep(1)
-                file_size = os.path.getsize(path)
-                if file_size != c:
-                    # Defer updates until the file has stopped changing size
-                    deferred_updates[path] = file_size
-                    continue
-                else:
-                    update_zip(path, zip_path)
-            else:
-                update_zip(path, zip_path)
+            update_backlog[path] = timestamp
+
+            # If most recent operation was updating a file, don't bother deleting
+            if path in delete_backlog:
+                delete_backlog.pop(path)
         
-        # Updates are grouped so that writes in quick succession don't spam zip calls
-        process_deferred_updates(deferred_updates, zip_path)
+        if timestamp - checkpoint > 5:
+            print '\nProcessing backlog...'
+            process_deferred_updates(update_backlog, delete_backlog, zip_path)
+            checkpoint = timestamp
 
-def process_deferred_updates(deferred_updates, zip_path):
+def process_deferred_updates(update_backlog, delete_backlog, zip_path):
     processed_paths = []
-    for path in deferred_updates:
-        print 'Checking deferred update: %s' % path
 
-        prev_file_size = deferred_updates[path]
-        cur_file_size = os.path.getsize(path)
-        if prev_file_size != cur_file_size:
-            print "Previous size: %s - Current size: %s" % (prev_file_size, cur_file_size)
-        else:
-            update_zip(path, zip_path) 
-            processed_paths.append(path)
+    for path in update_backlog:
+        update_zip(path, zip_path) 
+        processed_paths.append(path)
 
-    for path in processed_paths:
-        deferred_updates.pop(path)
+    for path in delete_backlog:
+        delete_zip(path, zip_path) 
+        processed_paths.append(path)
+
+    update_backlog = {}
+    delete_backlog = {}
 
 def sync_zip(zip_path):
     update_args = ['zip', '--symlink', 'FSr', zip_path, '.']
--- tests/kodethon/test.py
+++ tests/kodethon/test.py
@@ -7,189 +7,196 @@ import shutil
 
 from random import *
 
-def check_file_created(container_file_path, is_dir, zip_path, iteration = 0):
-    ''' Check file exists in zip file '''
-    time.sleep(iteration + 1)
-    zf = zipfile.ZipFile(zip_path, 'r')
-    paths_in_zip = zf.namelist()
-    try:
-        found = paths_in_zip.index(container_file_path if not is_dir else container_file_path + '/')
-        print "Test succeeded after iteration %s" % iteration
-    except ValueError:
-        check_file_created(container_file_path, is_dir, zip_path, iteration + 1)
+class Tester:
+    
+    def __init__(self, test_dir, zip_path):
+        self.test_dir = test_dir
+        self.zip_path = zip_path
+        self.update_list = []
+        self.delete_list = []
+        self.dir_list = [test_dir]
+        self.file_list = []
 
-def check_file_deleted(container_file_path, is_dir, zip_path, iteration = 0):
-    ''' Check file does not exist in zip file '''
-    time.sleep(iteration + 1)
-    zf = zipfile.ZipFile(zip_path, 'r')
-    paths_in_zip = zf.namelist()
-    try:
-        found = paths_in_zip.index(container_file_path if not is_dir else container_file_path + '/')
-        check_file_deleted(container_file_path, is_dir, zip_path, iteration + 1)
-    except ValueError:
-        print "Test succeeded after iteration %s" % iteration
+    def to_container_path(self, path):
+        return path.replace(self.test_dir + '/', '', 1)
 
-def check_file_updated(container_file_path, text, zip_path, iteration = 0):
-    ''' Check file in zip exists and its contents match text '''
-    time.sleep(iteration + 1)
-    zf = zipfile.ZipFile(zip_path, 'r')
-    try:
-        data = zf.read(container_file_path)
-    except KeyError:
-        return check_file_updated(container_file_path, text, zip_path, iteration + 1)
+    def check_file_created(self, path, is_dir, iteration = 0):
+        ''' Check file exists in zip file '''
+        container_file_path = self.to_container_path(path)
+        time.sleep(iteration + 1)
+        zf = zipfile.ZipFile(self.zip_path, 'r')
+        paths_in_zip = zf.namelist()
+        try:
+            found = paths_in_zip.index(container_file_path if not is_dir else container_file_path + '/')
+            print "Test succeeded after iteration %s" % iteration
+        except ValueError:
+            self.check_file_created(container_file_path, is_dir, iteration + 1)
 
-    if data == text:
-        print "Test succeeded after iteration %s" % iteration
-    else:
-        print "Zip data has length: %s while expected length is: %s" % (len(data), len(text))
-        return check_file_updated(container_file_path, text, zip_path, iteration + 1)
+    def check_file_deleted(self, path, is_dir, iteration = 0):
+        ''' Check file does not exist in zip file '''
+        container_file_path = self.to_container_path(path)
+        time.sleep(iteration + 1)
+        zf = zipfile.ZipFile(self.zip_path, 'r')
+        paths_in_zip = zf.namelist()
+        try:
+            found = paths_in_zip.index(container_file_path if not is_dir else container_file_path + '/')
+            self.check_file_deleted(container_file_path, is_dir, iteration + 1)
+        except ValueError:
+            print "Test succeeded after iteration %s" % iteration
 
-def pick_random_file(file_list):
-    if len(file_list) == 0:
-        return None
-    return file_list[randint(0, len(file_list) - 1)]
+    def check_file_updated(self, path, text, iteration = 0):
+        ''' Check file in zip exists and its contents match text '''
+        container_file_path = self.to_container_path(path)
+        time.sleep(iteration + 1)
+        zf = zipfile.ZipFile(self.zip_path, 'r')
+        try:
+            data = zf.read(container_file_path)
+        except KeyError:
+            return self.check_file_updated(container_file_path, text, iteration + 1)
 
-def pop_random_file(file_list):
-    index = randint(0, len(file_list) - 1)
-    f = file_list[index]
-    file_list.pop(index)
-    return f
+        if data == text:
+            print "Test succeeded after iteration %s" % iteration
+        else:
+            print "Zip data has length: %s while expected length is: %s" % (len(data), len(text))
+            return self.check_file_updated(container_file_path, text, iteration + 1)
 
-def create_test_case(test_dir, zip_path, dir_list, file_list):
-    char_set = string.ascii_lowercase + string.digits + '_-'
-
-    # Create a random dir path based on current structure
-    path = pick_random_file(dir_list)
-    filename = []
-    for _ in range(randint(1, 50)):
-        filename += char_set[randint(0, len(char_set) - 1)] 
-    path = os.path.join(path, ''.join(filename))
-
-    # Create the file or directory
-    is_dir = randint(0, 1) == 0
-    if is_dir:
-        print 'Creating folder: %s' % path
-        dir_list.append(path)
-        os.mkdir(path)
-    else:
-        print 'Creating file: %s' % path
-        fp = open(path, 'a')
-        file_list.append(path)
-
-    container_path = path.replace(test_dir + '/', '', 1)
-    check_file_created(container_path, is_dir, zip_path) 
-    return True
-
-def modify_test_case(test_dir, zip_path, dir_list, file_list):
-    char_set = string.ascii_lowercase + string.digits + '_-'
-
-    # Create a random dir path based on current structure
-    path = pick_random_file(dir_list)
-    filename = []
-    for _ in range(randint(1, 50)):
-        filename += char_set[randint(0, len(char_set) - 1)] 
-    path = os.path.join(path, ''.join(filename))
-
-    # If use existing, pick an existing file
-    use_existing = randint(0, 1) == 0
-    if not use_existing:
-        file_list.append(path)
-    else:
+    def pick_random_file(self, file_list):
         if len(file_list) == 0:
-            print 'No files to modify, continuing...'
-            return False
-        path = pick_random_file(file_list)
-        fp = open(path, 'rw+')
-        fp.truncate(0)
+            return None
+        return file_list[randint(0, len(file_list) - 1)]
+
+    def pop_random_file(self, file_list):
+        index = randint(0, len(file_list) - 1)
+        f = file_list[index]
+        file_list.pop(index)
+        return f
+
+    def create_test_case(self):
+        char_set = string.ascii_lowercase + string.digits + '_-'
+
+        # Create a random dir path based on current structure
+        path = self.pick_random_file(self.dir_list)
+        filename = []
+        for _ in range(randint(1, 50)):
+            filename += char_set[randint(0, len(char_set) - 1)] 
+        path = os.path.join(path, ''.join(filename))
+
+        # Create the file or directory
+        is_dir = randint(0, 1) == 0
+        if is_dir:
+            print 'Creating folder: %s' % path
+            self.dir_list.append(path)
+            os.mkdir(path)
+        else:
+            print 'Creating file: %s' % path
+            fp = open(path, 'a')
+            self.file_list.append(path)
+
+        self.check_file_created(path, is_dir) 
+
+    def modify_test_case(self):
+        char_set = string.ascii_lowercase + string.digits + '_-'
+
+        # Create a random dir path based on current structure
+        path = self.pick_random_file(self.dir_list)
+        filename = []
+        for _ in range(randint(1, 50)):
+            filename += char_set[randint(0, len(char_set) - 1)] 
+        path = os.path.join(path, ''.join(filename))
+
+        # If use existing, pick an existing file
+        use_existing = randint(0, 1) == 0
+        if not use_existing:
+            self.file_list.append(path)
+        else:
+            if len(self.file_list) == 0:
+                print 'No files to modify, continuing...'
+            else:
+                path = self.pick_random_file(self.file_list)
+                fp = open(path, 'rw+')
+                fp.truncate(0)
+                fp.close()
+
+        print 'Modifying file: %s' % path
+
+        fp = open(path, 'a')
+        num_chars = randint(0, 1000)
+        batch_size = randint(1, 100)
+        print 'Writing %s characters with a batch size of %s' % (num_chars, batch_size) 
+
+        text = ''
+        batch = ''
+        for i in xrange(0, num_chars):
+            char = char_set[randint(0, len(char_set) - 1)] 
+            batch += char
+            if i % batch_size == 0 or i == num_chars - 1:
+                fp.write(batch) 
+                batch = ''
+            text += char
         fp.close()
 
-    print 'Modifying file: %s' % path
+        self.check_file_updated(path, text)
 
-    fp = open(path, 'a')
-    num_chars = randint(0, 1000)
-    batch_size = randint(1, 100)
-    print 'Writing %s characters with a batch size of %s' % (num_chars, batch_size) 
-
-    text = ''
-    batch = ''
-    for i in xrange(0, num_chars):
-        char = char_set[randint(0, len(char_set) - 1)] 
-        batch += char
-        if i % batch_size == 0 or i == num_chars - 1:
-            fp.write(batch) 
-            batch = ''
-        text += char
-    fp.close()
-
-    container_path = path.replace(test_dir + '/', '', 1)
-    check_file_updated(container_path, text, zip_path)
-    return True
-
-def delete_test_case(test_dir, zip_path, dir_list, file_list):
-    is_dir = randint(0, 1) == 0
-    if is_dir:
-        path = pop_random_file(dir_list)
-        if path == test_dir:
-            print 'Root folder selected, continuing...'
-            dir_list.append(path)
-            return False
-        print 'Deleting folder: %s' % path
-        marked_files = []
-        marked_dirs = []
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in files:
-                marked_files.append(os.path.join(root, name))
-            for name in dirs:
-                marked_dirs.append(os.path.join(root, name))
-        shutil.rmtree(path)
-        container_path = path.replace(test_dir + '/', '', 1)
-        check_file_deleted(container_path, is_dir, zip_path)
-        
-        # Checked that children are also removed
-        for path in marked_files:
-            print 'Checking if file %s is deleted...' % path
-            container_path = path.replace(test_dir + '/', '', 1)
-            check_file_deleted(container_path, is_dir, zip_path)
-            file_list.remove(path)
-        for path in marked_dirs:
-            print 'Checking if folder %s is deleted...' % path
-            container_path = path.replace(test_dir + '/', '', 1)
-            check_file_deleted(container_path, is_dir, zip_path)
-            dir_list.remove(path)
-    else:
-        if len(file_list) == 0:
-            print 'No files to delete, continuing...'
-            return False 
-        path = pop_random_file(file_list)
-        print 'Deleting file: %s' % path
-        os.remove(path)
-        container_path = path.replace(test_dir + '/', '', 1)
-        check_file_deleted(container_path, is_dir, zip_path)
-    return True
+    def delete_test_case(self):
+        is_dir = randint(0, 1) == 0
+        if is_dir:
+            path = self.pop_random_file(self.dir_list)
+            if path == self.test_dir:
+                print 'Root folder selected, continuing...'
+                self.dir_list.append(path)
+            else:
+                print 'Deleting folder: %s' % path
+                marked_files = []
+                marked_dirs = []
+                for root, dirs, files in os.walk(path, topdown=False):
+                    for name in files:
+                        marked_files.append(os.path.join(root, name))
+                    for name in dirs:
+                        marked_dirs.append(os.path.join(root, name))
+                shutil.rmtree(path)
+                self.check_file_deleted(path, is_dir)
+                
+                # Checked that children are also removed
+                for path in marked_files:
+                    print 'Checking if file %s is deleted...' % path
+                    self.check_file_deleted(path, is_dir)
+                    self.file_list.remove(path)
+                for path in marked_dirs:
+                    print 'Checking if folder %s is deleted...' % path
+                    self.check_file_deleted(path, is_dir)
+                    self.dir_list.remove(path)
+        else:
+            if len(self.file_list) == 0:
+                print 'No files to delete, continuing...'
+            else:
+                path = self.pop_random_file()
+                print 'Deleting file: %s' % path
+                os.remove(path)
+                self.check_file_deleted(path, is_dir)
 
 def main():
     if len(sys.argv) < 4:
         print "USAGE: python test.py <TEST_DIR> <ZIP_PATH> <ITERATIONS>" 
         sys.exit(1)
 
+    events = ['IN_MODIFY', 'IN_DELETE', 'IN_CREATE']
+
     test_dir = sys.argv[1]
     zip_path = sys.argv[2]
     iterations  = int(sys.argv[3])
+    tester = Tester(test_dir, zip_path)
 
-    dir_list = [test_dir]
-    file_list = []
-    events = ['IN_MODIFY', 'IN_DELETE', 'IN_CREATE']
     for i in xrange(0, iterations):
         print ''
 
         # Pick an event and set it up
         event = events[randint(0, len(events) - 1)]
         if event == 'IN_CREATE':
-            create_test_case(test_dir, zip_path, dir_list, file_list) 
+            tester.create_test_case() 
         elif event == 'IN_DELETE':
-            delete_test_case(test_dir, zip_path, dir_list, file_list)
+            tester.delete_test_case()
         elif event == 'IN_MODIFY':       
-            modify_test_case(test_dir, zip_path, dir_list, file_list) 
+            tester.modify_test_case() 
         
 if __name__ == '__main__':
     main()
